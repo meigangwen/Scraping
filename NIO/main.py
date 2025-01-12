91d1de38a841c080bd07d32ed1d017ce931b6496
fancilivain <meigangwen@gmail.com>
2025-01-12 20:40:35 +0800

    Update main.py

--- NIO/main.py
+++ NIO/main.py
@@ -1,30 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import asyncio
-from pyppeteer import launch
-
-async def scrape(url):
-    # Launch a headless browser
-    browser = await launch(headless=True)
-    page = await browser.newPage()
-
-    # Navigate to the URL
-    await page.goto(url, {'waitUntil': 'networkidle0'})
-
-    # Wait for the necessary element to ensure it's loaded
-    await page.waitForSelector('body')
-
-    # Get the HTML content of the page
-    html_content = await page.content()
-    print(html_content)
-
-    # Close the browser
-    await browser.close()
+#from selenium.webdriver.common.by import By
+#from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.chrome.service import Service
+#from webdriver_manager.chrome import ChromeDriverManager
 
 if __name__ == '__main__':
     url = 'https://chargermap.nio.com/pe/h5/static/chargermap?channel=official'
-    asyncio.get_event_loop().run_until_complete(scrape(url))
+
 
 
 
