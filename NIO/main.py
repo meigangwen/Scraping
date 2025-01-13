849a5c6c8492b4a5a4e4b5c2518d60d71421af38
fancilivain <meigangwen@gmail.com>
2025-01-13 16:19:36 +0800

    Update main.py

--- NIO/main.py
+++ NIO/main.py
@@ -1,6 +1,5 @@
-#import requests
-#from bs4 import BeautifulSoup
 from selenium import webdriver
+
 
 if __name__ == '__main__':
     url = 'https://chargermap.nio.com/pe/h5/static/chargermap?channel=official'
@@ -9,9 +8,4 @@ if __name__ == '__main__':
 
     html_content = driver.page_source
     print(html_content)
-
     driver.quit()
-
-
-
-
