a140bff0dbcba5228f8c5a168d58269d391852af
fancilivain <meigangwen@gmail.com>
2025-01-13 13:17:07 +0800

    Update main.py

--- NIO/main.py
+++ NIO/main.py
@@ -1,13 +1,16 @@
-import requests
-from bs4 import BeautifulSoup
+#import requests
+#from bs4 import BeautifulSoup
 from selenium import webdriver
-#from selenium.webdriver.common.by import By
-#from selenium.webdriver.common.keys import Keys
-#from selenium.webdriver.chrome.service import Service
-#from webdriver_manager.chrome import ChromeDriverManager
 
 if __name__ == '__main__':
     url = 'https://chargermap.nio.com/pe/h5/static/chargermap?channel=official'
+    driver = webdriver.Chrome()
+    driver.get(url)
+
+    html_content = driver.page_source
+    print(html_content)
+
+    driver.quit()
 
 
 
